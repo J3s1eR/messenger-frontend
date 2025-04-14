1427551049b64828d20963f011522dcba74670c9
J3s1eR <nemov_alexander2001@mail.ru>
2025-04-14 05:45:49 +0300

    added user registration

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,7 @@
 import React, { createContext, useContext, useState } from 'react';
 import { apiService } from '../services/api/apiService';
+import CryptoService from '../services/crypto/CryptoService';
+import { v4 as uuidv4 } from 'uuid';
 
 interface AuthContextType {
   user: string | null;
@@ -25,17 +27,35 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
       console.log('Вход\nИмя:', username);
       console.log('Токен:', apiService.getToken());
 
-      const get_keys_response = await apiService.get('/user/keys')
-      console.log('Нужно ли возобновлять ключи:', get_keys_response.data.requiresReplenishing);
-      console.log('Ключи:', get_keys_response.data.currentKeys);
+      //const get_keys_response = await apiService.get('/user/keys')
+      //console.log('Нужно ли возобновлять ключи:', get_keys_response.data.requiresReplenishing);
+      //console.log('Ключи:', get_keys_response.data.currentKeys);
       
       const get_keys_response2 = await apiService.getUserKeys()
       console.log('Нужно ли возобновлять ключи:', get_keys_response2.data.requiresReplenishing);
       console.log('Ключи:', get_keys_response2.data.currentKeys);
 
-    } catch (error) {
-      console.error('Ошибка входа', error);
-      throw error;
+    } catch (error: any) {
+      if(error.response.status === 404 && error.response.data.message === 'User not registered') {
+        console.warn('Пользователь не зарегистрирован');
+
+        const data = {
+          identityPublicKey: CryptoService.generateBase64Key(),
+          signedPublicKey: CryptoService.generateBase64Key(),
+          oneTimeKeyList: Array.from({ length: 10 }, () => ({
+            number: uuidv4(),
+            oneTimeKey: CryptoService.generateBase64Key()
+          })),
+          userData: CryptoService.generateBase64Key(),
+          protectedSymmetricKey: CryptoService.generateBase64Key()
+        };
+
+        console.log('Регистрация пользователя:', data);
+        const register_user_response = await apiService.post('/user/register', data);
+        console.log('Регистрация пользователя, ответ:', register_user_response.data);
+      } else {
+        console.error('Ошибка при получении ключей:', error);
+      }
     }
   };
 
