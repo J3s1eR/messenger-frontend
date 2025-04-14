dbfaa42d97dd195b51c65a975e014df9bdcc8b92
J3s1eR <nemov_alexander2001@mail.ru>
2025-04-14 22:31:27 +0300

    main.tsx env options

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+//Доп. возможность установки окружения (по умолчанию используется pnpm run dev или pnpm run preview, или (- NODE_ENV=development или - NODE_ENV=production из docker-compose.yml))
+// Установка окружения перед импортами
+//window.__ENV__ = 'development'; // измените на 'production' при необходимости
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
