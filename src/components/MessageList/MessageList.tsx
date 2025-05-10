38dba02c1fbed9bc343a99f6595cb36bbae49683
J3s1eR <nemov_alexander2001@mail.ru>
2025-05-10 05:54:08 +0300

    fix: message list scroll to end

--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -23,8 +23,14 @@ export const MessageList = () => {
   // Отслеживаем прокрутку пользователем
   const handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
     //console.log(`MessageList.tsx:\handleScroll():\nscrolled`)
+    const messagesEndRefEl = messagesEndRef.current;
+    console.log(`___________________________`);
+    console.log(`MessageList/MessageList.tsx:\nscrollToBottom\nmessagesEndRefEl.getBoundingClientRect().bottom:`, messagesEndRefEl!.getBoundingClientRect().bottom);
     const target = event.currentTarget;
     const { scrollTop, scrollHeight, clientHeight } = target;
+    console.log(`MessageList/MessageList.tsx:\nscrollToBottom\scrollHeight:`, scrollHeight);
+    console.log(`MessageList/MessageList.tsx:\nscrollToBottom\nscrollHeight - messagesEndRefEl!.getBoundingClientRect().bottom:`, scrollHeight - messagesEndRefEl!.getBoundingClientRect().bottom);
+    //console.log(`MessageList.tsx:\handleScroll():\nscrollTop:`, scrollTop, `\nscrollHeight:`, scrollHeight, `\nclientHeight:`, clientHeight);
     const isAtBottom = scrollHeight - scrollTop - clientHeight < 100; // в пределах 100px от нижней границы
 
     //console.log(`MessageList.tsx:\handleScroll():\nsetUserScrolled(`, !isAtBottom, `)`)
@@ -60,17 +66,61 @@ export const MessageList = () => {
   const scrollToBottom = () => {
     // Найдем scrollable контейнер (это .content внутри CustomScrollbar)
     const scrollContainer = messagesEndRef.current?.parentElement?.parentElement;
-    if (scrollContainer) {
+    const messagesEndRefEl = messagesEndRef.current;
+    const messageInputRefEl = scrollContainer?.parentElement?.nextElementSibling
+
+
+    if (messagesEndRefEl && messageInputRefEl) {
+      const inputHeight = messageInputRefEl.clientHeight;
       // Прокручиваем до конца, но не заходя на поле ввода
-      const targetScroll = scrollContainer.scrollHeight - scrollContainer.clientHeight;
+      //const targetScroll = scrollContainer.scrollHeight - scrollContainer.clientHeight - inputHeight - messagesEndRefEl.getBoundingClientRect().bottom;
+      //const targetScroll = scrollContainer.scrollHeight - scrollContainer.clientHeight - (scrollContainer.scrollHeight - messagesEndRefEl.getBoundingClientRect().bottom);
       
-      // Используем smooth behavior
-      scrollContainer.scrollTo({
+      const targetScroll = messagesEndRefEl.getBoundingClientRect().bottom - scrollContainer.clientHeight + inputHeight;
+      console.log(`___________________________`);
+      console.log(`MessageList/MessageList.tsx:\nscrollToBottom\nscrollContainer.scrollHeight:`, scrollContainer.scrollHeight);
+      console.log(`MessageList/MessageList.tsx:\nscrollToBottom\nscrollContainer.scrollHeight - messagesEndRefEl.getBoundingClientRect().bottom:`, scrollContainer.scrollHeight - messagesEndRefEl.getBoundingClientRect().bottom);
+      console.log(`MessageList/MessageList.tsx:\nscrollToBottom\nscrollContainer.clientHeight:`, scrollContainer.clientHeight);
+      console.log(`MessageList/MessageList.tsx:\nscrollToBottom\ninputHeight:`, inputHeight);
+      console.log(`MessageList/MessageList.tsx:\nscrollToBottom\nscrollContainer.getBoundingClientRect().top:`, scrollContainer.getBoundingClientRect().top);
+      console.log(`MessageList/MessageList.tsx:\nscrollToBottom\nmessagesEndRefEl.getBoundingClientRect().top:`, messagesEndRefEl.getBoundingClientRect().top);
+      console.log(`MessageList/MessageList.tsx:\nscrollToBottom\nmessagesEndRefEl.getBoundingClientRect().bottom:`, messagesEndRefEl.getBoundingClientRect().bottom);
+      console.log(`MessageList/MessageList.tsx:\nscrollToBottom\ntargetScroll:`, targetScroll);
+
+      //const targetScroll = messagesEndRef.current?.offsetTop ?? 0;
+      //const targetScroll = messagesEndRef.current?.getBoundingClientRect().top ?? 0;
+      
+      scrollContainer.scrollBy({
         top: targetScroll,
-        behavior: 'smooth'
+        left: 0,
+
+        behavior: 'auto'
+
       });
+
+
+      // Используем smooth behavior
+      //messagesEndRefEl.scrollIntoView({
+      //  block: 'end',
+      //  behavior: 'smooth'
+      //});
+
+      //setTimeout(() => {
+      //  // Корректируем позицию прокрутки, чтобы не перекрывать инпут
+      //  const scrollOffset = inputHeight;
+      //  console.log(`MessageList/MessageList.tsx:\nscrollToBottom\nscrollOffset:`, scrollOffset);
+      //
+      //  if(scrollContainer){
+      //    scrollContainer.scrollBy({
+      //      top: scrollOffset, 
+      //      left: 0, 
+      //      behavior: 'smooth'
+      //    });
+      //  }
+      //}, 1000); // Добавляем задержку 
+
     }
-    setUserScrolled(false);
+    //setUserScrolled(false);
   };
 
   return (
@@ -112,6 +162,19 @@ export const MessageList = () => {
         
         <div ref={messagesEndRef} className={styles.messagesEndRef}/>
       </>}
+
+      {MessagesForChatWithContext.newMessagesCount > 0 && (
+        Array.from({ length: MessagesForChatWithContext.newMessagesCount }).map((_, index) => (
+          <MessageBubble
+            key={`empty-message-${index}`}
+            text=""
+            isOwn={false}
+            isFirstInGroup={false}
+            isLastInGroup={false}
+          />
+        ))
+      )}
+
     
     </div>
     
