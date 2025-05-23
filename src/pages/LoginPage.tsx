668ed7c92d2ccd96051804a405b6e198c0b61398
J3s1eR <nemov_alexander2001@mail.ru>
2025-05-23 16:27:40 +0300

    ui/ux: LoginPage and Emoji Category labels emprovements

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 
+import styles from "./LoginPage.module.css";
+import { Squircle } from '../components/ultimate-squircle/squircle-js';
+
 const LoginPage: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -19,23 +22,70 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <h2>Вход</h2>
-      <form onSubmit={handleLogin}>
-        <input 
-          type="text" 
-          placeholder="Логин" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)} 
-        />
-        <input 
-          type="password" 
-          placeholder="Пароль" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
-        />
-        <button type="submit">Войти</button>
+    <div className={styles.LoginPage}>
+      <div className={styles.LoginPage_Main}>
+      <h2 className={styles.LoginPage_Title}>Вход</h2>
+
+      <form 
+      className={styles.LoginPage_Form}
+      onSubmit={handleLogin}
+      >
+        <Squircle 
+          cornerRadius={20}
+
+          cornerSmoothing={1}
+          defaultWidth={500}
+          defaultHeight={60}
+          className={styles.inputContainer}
+
+          asChild
+        >
+          <div className={styles.inputWrapper}>
+            <input 
+              className={styles.input}
+              type="text" 
+              placeholder="Логин" 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)} 
+            />
+          </div>
+        </Squircle>
+
+        <Squircle 
+          cornerRadius={20}
+
+          cornerSmoothing={1}
+          defaultWidth={500}
+          defaultHeight={60}
+          className={styles.inputContainer}
+
+          asChild
+        >
+          <div className={styles.inputWrapper}>
+            <input 
+              className={styles.input}
+              type="password" 
+              placeholder="Пароль" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+            />
+          </div>
+        </Squircle>
+
+        <Squircle 
+          cornerRadius={20}
+
+          cornerSmoothing={1}
+          defaultWidth={120}
+          defaultHeight={60}
+          className={styles.ButtonContainer}
+
+          asChild
+        >
+          <button type="submit" className={styles.Button}>Войти</button>
+        </Squircle>
       </form>
+      </div>
     </div>
   );
 };
